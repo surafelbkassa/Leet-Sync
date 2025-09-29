94376054a41c936689f3e61bf1e0b84ebc6e77bc
SURAFEL BELACHEW KASSA <148704749+surafelbkassa@users.noreply.github.com>
2025-09-29 20:40:52 +0300

    Time: 0 ms (100%), Space: 17.7 MB (78.27%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        T = [0,1,1]
+        if n < 3:
+            return T[n]
+        for i in range(3,n+1):
+            T[0], T[1], T[2] = T[1],T[2],T[0]+T[1]+T[2]
+        return T[-1] 
+        
