e459ad48787be1040306053b1364eb7d7c6ebf58
SURAFEL BELACHEW KASSA <148704749+surafelbkassa@users.noreply.github.com>
2025-10-07 10:49:13 +0300

    Time: 111 ms (39.8%), Space: 19 MB (90.95%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -6,11 +6,11 @@ class Solution(object):
         """
         l,r=0,1
         ans = 0
-        while  r < len(prices):
-            if prices[r] < prices[l]:
-                l = r
-            else:
+        while r < len(prices):
+            if prices[l] < prices[r]:
                 ans = max(ans,prices[r] - prices[l])
+            else:
+                l = r
             r += 1
         return ans
-
+        
