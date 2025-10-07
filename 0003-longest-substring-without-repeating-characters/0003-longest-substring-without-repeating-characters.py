8b9c92d044dd4c8955daaad627cb161a3efdae45
SURAFEL BELACHEW KASSA <148704749+surafelbkassa@users.noreply.github.com>
2025-10-07 11:03:38 +0300

    Time: 21 ms (33.69%), Space: 17.9 MB (34.09%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,57 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # n=len(s)
-        # window=set()
-        # l=0
-        # res=0
-        # for r in range(n):
-        #     while s[r] in window:
-        #         window.remove(s[l])
-        #         l+=1
-        #     window.add(s[r])
-        #     res=max(res,r-l+1)
-        # return res
-        # char_set=set()
-        # l=0
-        # ans=0
-        # for r in range(len(s)):
-        #     while s[r] in char_set:
-        #         char_set.remove(s[l])
-        #         l+=1
-        #     char_set.add(s[r])
-        #     ans=max(ans,r-l+1)
-        # return ans
-        char = set()
-        l=0
-        ans =0
-        for i in range(len(s)):
-            while s[i] in char:
-                char.remove(s[l])
-                l+=1
-            ans = max(ans,i - l + 1)
-            char.add(s[i])
+        ans = 0
+        if len(s) == 1:
+            return 1
+        l,r = 0,0
+        seen = set()
+        while r < len(s):
+            if s[r] in seen:
+                while l < r and s[r] in seen:
+                    seen.remove(s[l])
+                    l += 1
+                seen.add(s[r])
+            else:
+                seen.add(s[r])
+            ans = max(ans,r-l+1)
+            r+=1
         return ans
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       
-            
-
-
-
-
-
-
-        
