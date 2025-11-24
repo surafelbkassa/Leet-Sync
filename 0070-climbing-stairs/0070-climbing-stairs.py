e48ef635821d482a9e3ae999c8f53df49c1201ba
SURAFEL BELACHEW KASSA <148704749+surafelbkassa@users.noreply.github.com>
2025-11-24 17:34:24 +0300

    Time: 0 ms (100%), Space: 17.7 MB (48.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,9 @@
 class Solution:
     memo = {}
     def climbStairs(self, n: int) -> int:
-        if n in [0,1]:
+        if n == 1 or n == 0:
             return 1
         if n in self.memo:
             return self.memo[n]
-        self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        self.memo[n] =  self.climbStairs(n-1)+self.climbStairs(n-2)
         return self.memo[n]
-
-        
-            
-        
