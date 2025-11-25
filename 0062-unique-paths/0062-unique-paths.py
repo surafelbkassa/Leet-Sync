da11419ea7ca87e0901162750b767b73ac93d50e
SURAFEL BELACHEW KASSA <148704749+surafelbkassa@users.noreply.github.com>
2025-11-25 08:02:45 +0300

    Time: 0 ms (100%), Space: 18.2 MB (5.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,18 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # n,m = 7,3
-        row = [1] * n
-        # row = [1] * 7
-        # row = [1,1,1,1,1,1,1] 
-        for i in range(m-1):
-        # for i in range(2),i =0,1
-            newRow = [1]*n
-            # newRow = [1,1,1,1,1,1,1]
-            for j in range(n-2,-1,-1):
-            # for j in range(5)-reverse order
-            #j = 5,4,3,2,1,0
-                newRow[j] = newRow[j+1] + row[j]
-                # newRow[5] = newRow[5+1] + row[5]
-            row = newRow
-        return row[0]
-
+        memo = {}
+        def count(i,j):
+            if i >= m or j >= n:
+                return 0
+            if i == m -1 and j == n - 1:
+                return 1
+            if (i,j) in memo:
+                return memo[(i,j)]
+            memo[(i,j)] = count(i+1,j) + count(i,j+1) 
+            return memo[(i,j)]
+        return count(0,0)
