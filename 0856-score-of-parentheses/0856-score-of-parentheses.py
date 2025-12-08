1fef7e114ae495e8dbc94de4103a7acfb9e87186
SURAFEL BELACHEW KASSA <surafelbkassa3@gmail.com>
2025-12-08 19:12:34 +0300

    Time: 0 ms (100%), Space: 17.6 MB (94.37%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack = [0]
+        for c in s:
+            if c == '(':
+                stack.append(0)
+            else:
+                n = stack.pop()
+                if n == 0:
+                    stack[-1]+= 1
+                else:
+                    stack[-1] += 2*n
+        return stack[-1]
+
+            
+        
