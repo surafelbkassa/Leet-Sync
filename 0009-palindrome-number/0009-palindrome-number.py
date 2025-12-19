e08da8650d5875dfa52acf5be488f1866ac0799a
SURAFEL BELACHEW KASSA <surafelbkassa3@gmail.com>
2025-12-19 21:36:26 +0300

    Time: 6 ms (66.4%), Space: 17.3 MB (99.98%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        i,j = 0, len(x) - 1
+        while i < j:
+            if x[i] != x[j]:
+                return False
+            i += 1
+            j -= 1
+        return True        
