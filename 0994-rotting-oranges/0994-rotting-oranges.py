01cc89f0efa094035a8cb35bd1be6dffa82166c1
SURAFEL BELACHEW KASSA <surafelbkassa3@gmail.com>
2026-02-15 19:38:05 +0300

    Time: 3 ms (77.9%), Space: 19.5 MB (30.06%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,40 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        q = deque()
+        fresh = 0
+        minutes = 0
+        
+        # Step 1: collect rotten and count fresh
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    q.append((r, c))
+                elif grid[r][c] == 1:
+                    fresh += 1
+        
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        
+        # Step 2: BFS
+        while q and fresh > 0:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    
+                    if (
+                        0 <= nr < rows and
+                        0 <= nc < cols and
+                        grid[nr][nc] == 1
+                    ):
+                        grid[nr][nc] = 2
+                        fresh -= 1
+                        q.append((nr, nc))
+            
+            minutes += 1
+        
+        return minutes if fresh == 0 else -1
